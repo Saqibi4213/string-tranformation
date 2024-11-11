a38722e08f8584921efd4f7c94d3d8acc2eaafec
Saqibi4213 <mehriamosawer@gmail.com>
2024-11-11 18:49:55 +0430

    some basic to advanced JS

---
+++ app.js
@@ -0,0 +1,146 @@
+// alert('Hello Mehria DO YOUR BESTTT!');
+// prompt('YES YOU CAN DO !');
+// console.log('hello');
+// document.write('<h1>hello</h1>');
+// document.write('<h1>hola!</h1>');
+// confirm('do you want to learn js seriously !?');
+// var name = 'mehria';
+// alert(name);
+// let name = 'mehr';
+// document.write(name);
+// document.querySelectorAll('main').innerHTML = '<h2>Hello Again!</h2>';
+// document.getElementsByClassName('main')[0].innerHTML = 'welomee';
+// document.getElementsByClassName('main')[1].innerHTML = 'homee';
+// let a = 'hey';
+// let b = 'hola';
+// let c = a + ' ' + b;
+// alert(c);
+// time of break start 53: 59 
+// we will come again tommorow !
+// bye !
+// var a = '3' + '2';
+// alert(a);
+// let a = 3;
+// a++;
+// document.querySelector('.main').innerHTML = a;
+// let x = 3;
+// let y = 3;
+// z = x <=  y;
+// document.querySelector('.main').innerHTML = z;  
+
+// let a = false;
+// let b = false;
+// let z = a && b;
+// document.querySelector('.main').innerHTML = z;
+// let a = false;
+// let b = false;
+// let z = a || b;
+// document.querySelector('.main').innerHTML = z;
+
+// let a = 7;
+// let b = 9;
+// z = a < b ? 'yes' : 'no';
+// document.querySelector('.main').innerHTML = z;
+
+// let a = 7;
+// let z = 9;
+// z += a;
+// document.querySelector('.main').innerHTML = z;
+
+// fncss
+
+// function greet (click) {
+//     let greeting = document.getElementById('name').value;
+//     let name = 'hey guys' + ' ' + greeting;
+//     document.getElementsByClassName('greet')[0].innerHTML = name;
+// }
+
+// function cal() {
+//     var opr1 = document.getElementById('first').value;
+//     var opr2 = document.getElementById('second').value;
+//     var ope = document.getElementById('ope').value;
+//     if(ope == '+'){
+//         var res = parseInt(opr1) + parseInt(opr2);
+//     }
+//     if(ope == '-'){
+//         var res = parseInt(opr1) - parseInt(opr2);
+//     }
+//     if(ope == '*'){
+//         var res = parseInt(opr1) * parseInt(opr2);
+//     }
+//     if(ope == '/'){
+//         var res = parseInt(opr1) / parseInt(opr2);
+//     }
+//     document.getElementById('res').value = res;
+// }
+
+// function call(){
+//     var result = prompt('Enter Your Name', '');
+//     document.getElementById('res').innerHTML = result;
+// }
+
+// let  vote = 19;
+// if(vote >= 18){
+//     document.write('yes you can vote!')
+// }else if(vote === 17){
+//     document.write('plz wait ine more year')
+// }
+// else{
+//     document.write('no you cant vote!')
+// }
+
+// let cars = [];
+// cars = ['audi', 'bmw', 'mercedes', 'toyota'];
+// document.write(cars[1]);
+
+// var cars = new Array();
+// cars = ['audi', 'bmw', 'mercedes', 'toyota'];
+// cars[4] = 'hjfghjbh';
+// document.write(cars[4]);
+
+// for (var i = 0; i =11;i++){
+    
+// }
+
+// var i = 0;
+// while(i<20) {
+//     document.write('mehr<br/>');
+//     i++;
+// }
+
+// var i = 20;
+// while(i>0) {
+//     document.write(i+ ' ','mehr<br/>');
+//     i--;
+// }
+
+// var cars = ['car1', 'car2', 'car3', 'car4', 'car5']
+
+// cars.forEach(car);
+
+// function car(item, index, array){
+//     document.write(index+' => '+item+'<br/>');
+//     array[index] = item+ '8';
+// }
+
+
+// cars.forEach(carrr);
+// function carrr(item, index){
+//     document.write(index+' => '+item+'<br/>');
+// }
+
+// for(var i = 0; i <10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     document.write(i, '<br/>');
+// }
+
+// document.write(i, '<hr/>');
+
+// for(var i = 0; i <10; i++){
+//     if(i == 5){
+//         continue;
+//     }
+//     document.write(i, '<br/>');
+// }
